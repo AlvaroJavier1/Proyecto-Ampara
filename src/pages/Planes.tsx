35ba4bdcbede15b4263d9f150c61970850167346
Javier <mendezalvarojavier1@gmail.com>
2026-07-30 10:59:28 -0600

    feat: agregar paginas del sitio web

---
+++ src/pages/Planes.tsx
@@ -0,0 +1,7 @@
+export function Planes() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-surface">
+            <h1 className="text-4xl font-display text-primary">Proximamente !!</h1>
+        </div>
+    )
+}
