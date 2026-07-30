b2314c3e12fe47daaa497cd0155a0585e140df3c
Javier <mendezalvarojavier1@gmail.com>
2026-07-30 11:05:48 -0600

    refactor(App.tsx): agregar importaciones de pages al sitio web

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { RootLayout } from '@/layouts/RootLayout'
 import { Home } from '@/pages/Home'
+import { Nosotros } from './pages/Nosotros'
+import { Vida } from './pages/Vida'
+import { Danos } from './pages/Danos'
+import { Auto } from './pages/Auto'
+import { Planes } from './pages/Planes'
+import { Contacto } from './pages/Contacto'
 
 function App() {
   
   return (
     <BrowserRouter basename="/Proyecto-Ampara">
       <Routes>
-        <Route index element={<Home/>}/>
+        <Route path='/' element={<RootLayout/>}>
+          <Route index element={<Home/>}/>
+          <Route path='/nosotros' element={<Nosotros/>}/>
+          <Route path='/vida' element={<Vida/>}/>
+          <Route path='/danos' element={<Danos/>}/>
+          <Route path="/auto" element={<Auto />} />
+          <Route path="/planes" element={<Planes />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
